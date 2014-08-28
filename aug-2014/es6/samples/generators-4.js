f2b90d6da2af1aee53de1e38c233dfa98a565340
Thomas Hopkins <thopkins@redventures.com>
2014-08-27 23:26:54 -0400

    add clearer callbacks example

--- aug-2014/es6/samples/generators-4.js
+++ aug-2014/es6/samples/generators-4.js
@@ -1,8 +1,28 @@
 var fs = require('fs');
+var redis = require('redis');
+var db = require('redis').createClient();
 fs.readFile('package.json', function (err, buf) {
   if (err) {
     return console.error('fail', e.stack);
   }
 
-  //do something with buf here
+  //oh, I need to save this to the db
+  db.sadd('savedFiles', 'package.json', 
+    function (err, res) {
+      if (err) {
+        return console.error('ouch.');
+      }
+
+      //now I need to copy it somewhere
+      var oldFile = fs.createReadStream('package.json');
+      var newFile = fs.createWriteStream('package.json.backup');
+
+      oldFile.on('end', function () {
+        console.log('file copied!');
+        setImmediate(process.exit);
+      });
+
+      oldFile.pipe(newFile);
+    }
+  );
 });
